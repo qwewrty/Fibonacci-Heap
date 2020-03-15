33944f7367471441b499d1d308ff5a191e77bab2
Sandeep S <sandeep.carmel@gmail.com>
2020-03-14 20:19:17 -0400

    Added comments to the java files

--- MaxFibHeap.java
+++ MaxFibHeap.java
@@ -1,211 +1,330 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Class MaxFibHeap.
+ * 
+ * This class Implements the max fibonacci heap with some of the supported
+ * operations. All operations implemented here are an implementation of
+ * fibonacci heap as explained in the CLRS textbook.
+ * 
+ * @author Sandeep S
+ */
 public class MaxFibHeap {
-    // Can handle almost upto a 100 Million nodes. => log(100000000) = 26.something
-    private static final int DEGREE_TABLE_SIZE = 26;
-    public static final class Node {
-        private int priority;
-        private String data;
-        private Node leftSibling;
-        private Node rightSibling;
-        private Node parent;
-        private Node child;
-        private int degree;
-        private boolean childCut;
 
-        public Node(String data, int priority) {
-            leftSibling = rightSibling = this;
-            this.data = data;
-            this.priority = priority;
-            this.degree = 0;
-            this.parent = null;
-            this.child = null;
-            this.childCut = false;
-        }
+	/**
+	 * The Constant DEGREE_TABLE_SIZE.
+	 * 
+	 * Can handle almost 100 Million nodes. => log(100000000) = 26.something.
+	 */
+	private static final int DEGREE_TABLE_SIZE = 26;
 
-        public int getPriority() {
-            return priority;
-        }
+	/**
+	 * The Class Node.
+	 */
+	public static final class Node {
 
-        public String getData() {
-            return data;
-        }
-    }
+		/** The priority. */
+		private int priority;
 
-    Node max = null;
-    int noOfNodes = 0;
+		/** The data. */
+		private String data;
 
-    private static Node mergeSiblingLists(Node node1, Node node2) {
-        if(null == node1) {
-            return node2;
-        }
-        if(null == node2) {
-            return node1;
-        }
+		/** The left sibling. */
+		private Node leftSibling;
 
-        Node temp = node1.rightSibling;
-        node1.rightSibling = node2.rightSibling;
-        node1.rightSibling.leftSibling = node1;
-        node2.rightSibling = temp;
-        node2.rightSibling.leftSibling = node2;
-        return node1.priority > node2.priority ? node1 : node2;
-    }
+		/** The right sibling. */
+		private Node rightSibling;
 
-    public void insert(Node node) {
-        // Set default values.
-        node.degree = 0;
-        node.child = null;
-        node.parent = null;
-        node.childCut = false;
-        node.rightSibling = node.leftSibling = node;
-        // Increment number of nodes.
-        ++noOfNodes;
+		/** The parent. */
+		private Node parent;
 
-        //Recalculate max.
-        if(null == max) {
-            max = node;
-            return;
-        }
-        max = mergeSiblingLists(node, max);
-    }
+		/** The child. */
+		private Node child;
 
-    public Node extractMax() {
-        Node toReturn = max;
-        if(max == null) {
-            throw new RuntimeException("No nodes in the tree!");
-        }
+		/** The degree. */
+		private int degree;
 
-        Node child = max.child;
-        if(null != child) {
-            // Move all children of max to the root list.
-            while(child.parent != null) {
-                child.parent = null;
-                child = child.rightSibling;
-            }
-            mergeSiblingLists(child, max);
-        }
+		/** The child cut. */
+		private boolean childCut;
 
-        // remove max from the root list.
-        if(max.rightSibling == max) {
-            max = null;
-        } else {
-            max.rightSibling.leftSibling = max.leftSibling;
-            max.leftSibling.rightSibling = max.rightSibling;
-            max = max.rightSibling;
-        }
+		/**
+		 * Instantiates a new node.
+		 *
+		 * @param data     the data
+		 * @param priority the priority
+		 */
+		public Node(String data, int priority) {
+			leftSibling = rightSibling = this;
+			this.data = data;
+			this.priority = priority;
+			this.degree = 0;
+			this.parent = null;
+			this.child = null;
+			this.childCut = false;
+		}
 
-        // Degree wise merge of remaining nodes
-        pairwiseCombine();
+		/**
+		 * Gets the priority.
+		 *
+		 * @return the priority
+		 */
+		public int getPriority() {
+			return priority;
+		}
 
-        // Decrement no. of nodes.
-        --noOfNodes;
+		/**
+		 * Gets the data.
+		 *
+		 * @return the data
+		 */
+		public String getData() {
+			return data;
+		}
+	}
 
-        return toReturn;
-    }
+	/** The pointer to the node with the max value in the heap. */
+	Node max = null;
 
-    public void increaseKey(Node node, int increasedPriority) {
-        if(increasedPriority <= node.priority ) {
-            throw new RuntimeException("Invalid increaseKey");
-        }
+	/** The no of nodes in the heap. */
+	int noOfNodes = 0;
 
-        node.priority = increasedPriority;
-        Node parent = node.parent;
-        if(parent != null && node.priority > parent.priority) {
-            cut(node, parent);
-            cascadingCut(parent);
-        }
-        if(node.priority > max.priority) {
-            max = node;
-        }
-    }
+	/**
+	 * Merges two separate circular doubly linked lists into one.
+	 *
+	 * @param node1 A node from the first list.
+	 * @param node2 A node from the second list.
+	 * @return The node with a higher priority(i.e max of node1 and node2).
+	 */
+	private static Node mergeSiblingLists(Node node1, Node node2) {
+		if (null == node1) {
+			return node2;
+		}
+		if (null == node2) {
+			return node1;
+		}
 
-    private void cut(Node node, Node parent) {
-        //Remove node from the child list of the parent
-        if(node.rightSibling == node) {
-            parent.child = null;
-            parent.degree = 0;
-        } else {
-            node.rightSibling.leftSibling = node.leftSibling;
-            node.leftSibling.rightSibling = node.rightSibling;
-            if(parent.child == node) {
-                parent.child = node.rightSibling;
-            }
-            parent.degree--;
-        }
-        // Prepare for merging with root list.
-        node.parent = null;
-        node.leftSibling = node.rightSibling = node;
-        node.childCut = false;
+		// Since both are non empty have to do a crisscross connection between the nodes
+		// and next elements in each list.
+		Node temp = node1.rightSibling;
+		node1.rightSibling = node2.rightSibling;
+		node1.rightSibling.leftSibling = node1;
+		node2.rightSibling = temp;
+		node2.rightSibling.leftSibling = node2;
+		// return the max of the 2 nodes.
+		return node1.priority > node2.priority ? node1 : node2;
+	}
 
-        // Merge with root list
-        max = mergeSiblingLists(node, max);
-    }
+	/**
+	 * Insert.
+	 *
+	 * @param node the node
+	 */
+	public void insert(Node node) {
+		// Set default values.
+		node.degree = 0;
+		node.child = null;
+		node.parent = null;
+		node.childCut = false;
+		// Done so that a single node is also considered as a circular list;
+		node.rightSibling = node.leftSibling = node;
+		// Increment number of nodes.
+		++noOfNodes;
 
-    private void cascadingCut(Node node) {
-        Node parent = node.parent;
-        if(null == parent) {
-            return;
-        }
-        if(node.childCut) {
-            cut(node, parent);
-            cascadingCut(parent);
-        } else {
-            node.childCut = true;
-        }
-    }
+		// Recalculate max.
+		if (null == max) {
+			max = node;
+			return;
+		}
+		// Merge the node into the root list.
+		max = mergeSiblingLists(node, max);
+	}
 
-    private void pairwiseCombine() {
-        //Use array list as we are building a priority queue so using a HashMap inside doesn't make sense.
-        List<Node> degreeTable = new ArrayList<>(DEGREE_TABLE_SIZE);
-        // Initialize degree table.
-        for (int i=0; i<DEGREE_TABLE_SIZE; i++) {
-            degreeTable.add(null);
-        }
+	/**
+	 * Extract max.
+	 *
+	 * @return The node with the highest priority.
+	 */
+	public Node extractMax() {
+		Node toReturn = max;
+		if (max == null) {
+			throw new RuntimeException("No nodes in the tree!");
+		}
 
-        List<Node> nodesToCheck = new ArrayList<>();
-        for(Node temp = max; nodesToCheck.isEmpty() || nodesToCheck.get(0) != temp; temp = temp.rightSibling) {
-            nodesToCheck.add(temp);
-        }
+		Node child = max.child;
+		if (null != child) {
+			// Move all children of max to the root list.
+			while (child.parent != null) {
+				child.parent = null;
+				child = child.rightSibling;
+			}
+			// Merging the child list with the root list.
+			mergeSiblingLists(child, max);
+		}
 
-        for(Node currentNode : nodesToCheck) {
-            while(true) {
-                int degree = currentNode.degree;
-                // Just in case degree goes beyond the initial table size.
-                while (currentNode.degree >= degreeTable.size()) {
-                    degreeTable.add(null);
-                }
+		// Remove max from the root list.
+		if (max.rightSibling == max) {
+			max = null;
+		} else {
+			max.rightSibling.leftSibling = max.leftSibling;
+			max.leftSibling.rightSibling = max.rightSibling;
+			max = max.rightSibling;
+		}
 
-                Node previousSameDegreeNode = degreeTable.get(degree);
-                if(null == previousSameDegreeNode) {
-                    degreeTable.set(degree, currentNode);
-                    break;
-                }
-                degreeTable.set(degree, null);
+		// Degree wise merge of remaining nodes.
+		pairwiseCombine();
 
-                Node max = previousSameDegreeNode.priority < currentNode.priority ? currentNode : previousSameDegreeNode;
-                Node min = previousSameDegreeNode.priority < currentNode.priority ? previousSameDegreeNode : currentNode;
+		// Decrement number of nodes.
+		--noOfNodes;
 
-                // Yank min from the root list and make it a child of max
-                min.rightSibling.leftSibling = min.leftSibling;
-                min.leftSibling.rightSibling = min.rightSibling;
+		return toReturn;
+	}
 
-                min.leftSibling = min.rightSibling = min;
-                min.parent = max;
-                max.child = mergeSiblingLists(min, max.child);
+	/**
+	 * Increase key.
+	 * 
+	 * Used to increase the priority of a node.
+	 *
+	 * @param node              the node
+	 * @param increasedPriority the increased priority
+	 */
+	public void increaseKey(Node node, int increasedPriority) {
+		if (increasedPriority <= node.priority) {
+			throw new RuntimeException("Invalid increaseKey");
+		}
 
-                min.childCut = false;
+		node.priority = increasedPriority;
+		Node parent = node.parent;
+		// Check if the increased priority is greater than its parent's priority.
+		if (parent != null && node.priority > parent.priority) {
+			cut(node, parent);
+			cascadingCut(parent);
+		}
 
-                max.degree++;
+		// Make sure the max pointer is still correct.
+		if (node.priority > max.priority) {
+			max = node;
+		}
+	}
 
-                currentNode = max;
-            }
+	/**
+	 * Cut.
+	 * 
+	 * Performs the cut operation.
+	 *
+	 * @param node   the node
+	 * @param parent the parent
+	 */
+	private void cut(Node node, Node parent) {
+		// Remove node from the child list of the parent
+		if (node.rightSibling == node) {
+			// Parent no more has a child.
+			parent.child = null;
+			parent.degree = 0;
+		} else {
+			// Yank the child node out from the children list.
+			node.rightSibling.leftSibling = node.leftSibling;
+			node.leftSibling.rightSibling = node.rightSibling;
+			// Change the parent's child pointer if needed.
+			if (parent.child == node) {
+				parent.child = node.rightSibling;
+			}
+			// Decrement the degree of the parent.
+			parent.degree--;
+		}
+		// Prepare for merging with root list.
+		node.parent = null;
+		node.leftSibling = node.rightSibling = node;
+		node.childCut = false;
 
-            if(currentNode.priority > max.priority) {
-                max = currentNode;
-            }
-        }
-    }
+		// Merge with root list
+		max = mergeSiblingLists(node, max);
+	}
 
+	/**
+	 * Cascading cut.
+	 *
+	 * @param node the node
+	 */
+	private void cascadingCut(Node node) {
+		Node parent = node.parent;
+		if (null == parent) {
+			return;
+		}
+		// If childCut is true perform the cut operation.
+		if (node.childCut) {
+			cut(node, parent);
+			cascadingCut(parent);
+		} else {
+			// Mark that the node had its child cut once.
+			node.childCut = true;
+		}
+	}
 
+	/**
+	 * Pairwise combine.
+	 * 
+	 * Performs the degreewise merge of the nodes in the fibonacci heap.
+	 */
+	private void pairwiseCombine() {
+		// Use array list as we are building a priority queue so using a HashMap inside
+		// doesn't make sense.
+		List<Node> degreeTable = new ArrayList<>(DEGREE_TABLE_SIZE);
+		// Initialize degree table.
+		for (int i = 0; i < DEGREE_TABLE_SIZE; i++) {
+			degreeTable.add(null);
+		}
+
+		// Have to check all nodes in the root list.
+		List<Node> nodesToCheck = new ArrayList<>();
+		for (Node temp = max; nodesToCheck.isEmpty() || nodesToCheck.get(0) != temp; temp = temp.rightSibling) {
+			nodesToCheck.add(temp);
+		}
+
+		for (Node currentNode : nodesToCheck) {
+			while (true) {
+				int degree = currentNode.degree;
+				// Just in case degree goes beyond the initial table size.
+				while (currentNode.degree >= degreeTable.size()) {
+					degreeTable.add(null);
+				}
+
+				Node previousSameDegreeNode = degreeTable.get(degree);
+				// If there was a node of same degree then merge.
+				if (null == previousSameDegreeNode) {
+					degreeTable.set(degree, currentNode);
+					break;
+				}
+
+				// Take the node of same degree
+				degreeTable.set(degree, null);
+
+				// Find max of the two.
+				Node max = previousSameDegreeNode.priority < currentNode.priority ? currentNode
+						: previousSameDegreeNode;
+				Node min = previousSameDegreeNode.priority < currentNode.priority ? previousSameDegreeNode
+						: currentNode;
+
+				// Yank min from the root list and make it a child of max
+				min.rightSibling.leftSibling = min.leftSibling;
+				min.leftSibling.rightSibling = min.rightSibling;
+				min.leftSibling = min.rightSibling = min;
+				min.parent = max;
+				max.child = mergeSiblingLists(min, max.child);
+
+				min.childCut = false;
+
+				// Increment the degree of max.
+				max.degree++;
+
+				// Redo the operation for the newly created tree.
+				currentNode = max;
+			}
+
+			// Making sure the pointer points to the max.
+			if (currentNode.priority > max.priority) {
+				max = currentNode;
+			}
+		}
+	}
 }
--- hashtagcounter.java
+++ hashtagcounter.java
@@ -3,7 +3,13 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.*;
 
+/**
+ * The Class hashtagcounter.
+ * 
+ * @author Sandeep S
+ */
 public class hashtagcounter {
+    
     public static void main(String[] args) {
         if(args.length < 1) {
             throw new RuntimeException("Please provide input file as first argument.");
@@ -12,35 +18,50 @@ public class hashtagcounter {
         try (Scanner scanner = new Scanner(new File(args[0]))) {
             PrintStream fileOut = new PrintStream("output_file.txt");
             System.setOut(fileOut);
+            //Initiate hash map to store hashtag and its node.
             Map<String, MaxFibHeap.Node> hashtagMap = new HashMap<>();
+            // Initiate the max fibonacci heap.
             MaxFibHeap fibHeap = new MaxFibHeap();
 
+            // Start reading the file
             while(scanner.hasNext()) {
                 String line = scanner.nextLine();
+
                 if(line.startsWith("#")) {
+                    // Line is a hashtag.
                     String[] arguments = line.split(" ");
                     String hashTag = arguments[0].substring(1);
                     int count = Integer.parseInt(arguments[1]);
+                    // Check if its a recurring hashtag or a new one.
                     if(hashtagMap.containsKey(hashTag)) {
+                        // Increase key if its recurring.
                         MaxFibHeap.Node node = hashtagMap.get(hashTag);
                         fibHeap.increaseKey(node, node.getPriority() + count );
                     } else {
+                        // Insert if its new.
                         MaxFibHeap.Node node = new MaxFibHeap.Node(hashTag, count);
                         fibHeap.insert(node);
                         hashtagMap.put(hashTag, node);
                     }
                 } else if(line.equalsIgnoreCase("stop")) {
+                    // End the reading.
                     break;
                 } else {
+                    // It is a query and has an integer.
                     int requiredNoOfHashTags = Integer.parseInt(line);
+                    // List of nodes to reinsert.
                     List<MaxFibHeap.Node> nodesToReinsert = new LinkedList<>();
+                    // Array to store the hastags as a string and print.
                     String[] hashTags = new String[requiredNoOfHashTags];
+                    // performing extract min operation requiredNoOfHashTags number of times.
                     for(int i=0;i<requiredNoOfHashTags;i++) {
                         MaxFibHeap.Node max = fibHeap.extractMax();
                         hashTags[i] = max.getData();
                         nodesToReinsert.add(max);
                     }
+                    // Print out the tags in the array seperated by commas.
                     System.out.println(String.join(",", hashTags));
+                    // Reinsert the removed nodes.
                     for(MaxFibHeap.Node node : nodesToReinsert) {
                         fibHeap.insert(node);
                     }
